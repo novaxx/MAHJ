edf2d8367611dda2dc6cab4c48c9ef982f48765f
zhangxx <zhangxx@gionee.com>
2017-07-20 17:34:23 +0800

    调整接口
    Change-Id: Ibfce255b0bbb3a5d4e4e98fe85b73ca31afbba70

--- nova/common/game/mahjong/util/MahjGameCommand.java
+++ nova/common/game/mahjong/util/MahjGameCommand.java
@@ -5,7 +5,7 @@ public class MahjGameCommand {
 	// 快速游戏
 	public static final int REQUEST_GAME_START = 1201;
 	// 开始游戏
-	public static final int REQUEST_ROOM_GAME_START = 1202;
+	public static final int REQUEST_GAME_ROOM_START = 1202;
 	// 出牌
 	public static final int REQUEST_OUT_DATA = 1203;
 	// 操作
@@ -13,4 +13,7 @@ public class MahjGameCommand {
 
 	// 游戏信息变化
 	public static final int RESPONE_GAME_INFO_UPDATE = 1250;
+	
+	// 房间信息变化
+	public static final int RESPONE_ROOM_INFO_UPDATE = 1290;
 }
--- nova/common/room/RoomManager.java
+++ nova/common/room/RoomManager.java
@@ -66,7 +66,7 @@ public class RoomManager {
 		return mGameManager;
 	}
 
-	public void setHandler(RoomHandler handler) {
+	public void setRoomHandler(RoomHandler handler) {
 		mRoomHandler = handler;
 	}
 
